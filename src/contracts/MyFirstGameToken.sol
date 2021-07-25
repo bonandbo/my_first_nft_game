c929ebf58402c3c1f1230134ac9cd950838796f6
Diep Tran <dieptt88@gmail.com>
2021-07-25 22:48:58 +0700

    * WIP

---
+++ src/contracts/MyFirstGameToken.sol
@@ -0,0 +1,10 @@
+ pragma solidity ^0.8.6;
+
+import "./ERC721Full.sol";
+
+contract MyFirstGameToken is ERC721Full {
+    // Code
+}
+
+
+ 
---
+++ test/MyFirstGameToken.test.js
@@ -0,0 +1,9 @@
+const MyGame = artifacts.require('./MyFirstGameToken.sol')
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('My Game', (accounts) => {
+    // code
+})
